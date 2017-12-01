e59f075d9e0dca45bcbb276f6a54a9687ebd0d6a
Jimmy <jimmylin0179@gmail.com>
2017-12-02 01:22:22 +0800

    Demo:Varables and Constants

--- Hellocsharp/Program.cs
+++ Hellocsharp/Program.cs
@@ -11,6 +11,30 @@ namespace Hellocsharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            byte number=2;                    //var number=2
+            int count = 10;                   //var count=10
+            float totalPrice = 20.95f;        //loat totalPrice = 20.95f;  
+            char characater = 'A';            //var characater = 'A';
+            string fireName = "Mosh";         //var fireName = "Mosh";
+            bool isWorking = false;           //var isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);  ///cw tab兩下
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(characater);
+            Console.WriteLine(fireName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}",byte.MinValue,byte.MaxValue);
+            Console.WriteLine("{0} {1}",float.MinValue,float.MaxValue);
+
+            const float Pi = 3.14f;
+            
+
+
+
+
+
+            //ctrl + X  可以刪除該行
         }
     }
 }
