ccfb0414eec7bdcec01bfdff6e4de762af1bdd3c
Jimmy <jimmylin0179@gmail.com>
2017-12-12 20:15:24 +0800

    C#_basic_ch4 finish

--- 2/ch4/ch4/Program.cs
+++ 2/ch4/ch4/Program.cs
@@ -1,4 +1,5 @@
-﻿using ch4.math;
+﻿
+using ch4.math;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,12 @@ using System.Threading.Tasks;
 
 namespace ch4
 {
+    // 37:demo:Reference Types and Value Types
+    public class dog
+    {
+        public int Age;
+    }
+    //
     class Program
     {
         static void Main(string[] args)
@@ -46,6 +53,41 @@ c:\QQ\QQ";
             var aloha = @"Jimmy is so handsome";
             Console.WriteLine(text);
             Console.WriteLine(aloha);
+
+
+            //ch4: demo:reference types and values types
+            var a = 1;
+            var b = 2;
+            b++;
+            Console.WriteLine(string.Format("a:{0},b:{1}",a,b));
+
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(string.Format("array1[1]:{0}=array2[1]:{0}",array1[0],array2[0]));
+
+            //37 demo:reference types and values types
+            
+            var num = 1;
+            Increment(num);
+            Console.WriteLine(num);
+
+
+            var app = new dog() { Age = 20 };
+            MakeOld(app);
+            Console.WriteLine(app.Age);
+               
+            
+            
+        }
+        //下兩個都為37 demo:reference types and values types
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+        public static void MakeOld(dog dog)
+        {
+            dog.Age += 10;
         }
     }
 }
