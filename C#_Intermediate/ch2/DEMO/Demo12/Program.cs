31aaa78f5f57d31403df38a9019c469ea5d67dbf
MENG-TSE <jimmylin0179@gmail.com>
2018-01-23 01:03:02 +0800

    2018123

--- C#_Intermediate/ch2/DEMO/Demo12/Program.cs
+++ C#_Intermediate/ch2/DEMO/Demo12/Program.cs
@@ -10,6 +10,8 @@ namespace Demo12
     {
         static void Main(string[] args)
         {
+            var person = new Person(new DateTime(1995, 8, 10));
+            Console.WriteLine(person.Age);
         }
     }
 }
